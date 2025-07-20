ce609a6c272be1b9ff0f66a47aaf1b90c7e8f75f
bimaadam <rinbim.dev@gmail.com>
2025-07-20 21:21:54 +0700

    change folder to old

--- layout/main.php
+++ layout/main.php
@@ -15,7 +15,7 @@ if (!isset($_SESSION['user_id'])) {
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <title><?= $title ?? 'GraceFull Wedding - Dashboard' ?></title>
+    <title><?= $title ?? 'GraceFul Wedding - Dashboard' ?></title>
 
     <!-- Essential CSS -->
     <link rel="stylesheet" href="./assets/css/main.style.css">
